3256beca9cf7329ca7c6f0fef0ad28861ddd3fb7
José Rogério de Almeida Júnior <almeidajr.vr@gmail.com>
2026-03-20 17:11:17 -0300

    feat: integrate serilog logging

--- src/Kongroo.CloudGames.Api/Program.cs
+++ src/Kongroo.CloudGames.Api/Program.cs
@@ -1,6 +1,23 @@
+using System.Globalization;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSerilog(configuration =>
+    configuration
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
+        .Enrich.FromLogContext()
+        .Enrich.WithEnvironmentName()
+        .Enrich.WithEnvironmentUserName()
+        .Enrich.WithMachineName()
+        .Enrich.WithProcessId()
+        .Enrich.WithProcessName()
+        .Enrich.WithThreadId()
+        .Enrich.WithThreadName()
+        .Enrich.WithProperty("Application", AppDomain.CurrentDomain.FriendlyName)
+);
 
 var app = builder.Build();
 
