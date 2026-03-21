1bcc2449bdcdd874e047222c7ec00801a3bac090
José Rogério de Almeida Júnior <almeidajr.vr@gmail.com>
2026-03-21 10:26:32 -0300

    feat: implement Entity and IDomainEvent classes in SharedKernel

---
+++ src/Kongroo.SharedKernel/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace Kongroo.SharedKernel;
+
+public abstract class Entity<TId>(TId id)
+    where TId : IEquatable<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public TId Id { get; protected init; } = id;
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+}
---
+++ src/Kongroo.SharedKernel/IDomainEvent.cs
@@ -0,0 +1,3 @@
+﻿namespace Kongroo.SharedKernel;
+
+public interface IDomainEvent;
